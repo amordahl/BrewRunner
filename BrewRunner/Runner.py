175ebe4d7d73cd576870ad93064debb9ae9dbb7c
Austin Mordahl <austin.mordahL@utdallas.edu>
2020-01-14 16:50:59 +0000

    fix to timeout

--- BrewRunner/Runner.py
+++ BrewRunner/Runner.py
@@ -11,7 +11,7 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 BREW_PATH = "/home/reprodroid/reprodroid/amordahl-BREW/BREW/target/build/BREW-1.1.0-SNAPSHOT.jar"
-TIMEOUT = 60*60*60 # one hour
+TIMEOUT = 60*60 # one hour
 def run_brew(config_file):
     """
     Run a single instance of BREW with a config file.
@@ -29,7 +29,7 @@ def run_brew(config_file):
             r.set_generating_script(os.path.basename(config_file))
         logging.debug("Returning successfully to pool")
         return records
-    except TimeoutError:
+    except subprocess.TimeoutExpired:
         msg = f"Run with {config_file} timed out!"
         logging.critical(msg)
         with open('timeouts.txt', 'a') as f:
@@ -44,12 +44,14 @@ class Runner:
     """
     
     @staticmethod
-    def run_all_instances(brew_jar, config_list, jobs, parent_dir):
+    def run_all_instances(brew_jar, config_list, jobs, parent_dir, timeout):
         """
         Runs x BREW processes in parallel.
         """
         global BREW_PATH
         BREW_PATH = brew_jar
+        global TIMEOUT
+        TIMEOUT = timeout
         # break list into chunks of size jobs
         chunks = [config_list[i * jobs:(i+1) * jobs] for i in\
                   range((len(config_list) + jobs - 1) // jobs)]
--- BrewRunner/__main__.py
+++ BrewRunner/__main__.py
@@ -9,20 +9,24 @@ parser.add_argument('output_file')
 parser.add_argument('category_parent_directory', help='The parent directory, under which each directory in the benchmark is the category.')
 parser.add_argument('configs', nargs='+')
 parser.add_argument('-j', '--jobs', default=1, type=int)
+parser.add_argument('-t', '--timeout', default=60*60, type=int, help="timeout in seconds")
 args = parser.parse_args()
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
 def main():
+    # rlist is a list of lists
     for rlist in Runner.run_all_instances(args.brew_jar,
                                           args.configs,
                                           args.jobs,
-                                          args.category_parent_directory):
+                                          args.category_parent_directory,
+                                          args.timeout):
         logging.debug(str(rlist))
         for r in rlist:
-            r.set_category(args.category_parent_directory)
-        CSVWriter.write_to_csv(args.output_file, rlist)
+            for record in r:
+                record.set_category(args.category_parent_directory)
+            CSVWriter.write_to_csv(args.output_file, r)
 
 if __name__ == "__main__":
     main()
